890855c5459a80a8d9edcc91b3e7ce85c2232703
Joshua Caron <joshua@joshuacaron.ca>
2016-04-23 20:48:11 -0400

    Fixed missing file references

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,33 +2,14 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.Base.extend({
-
-  prompting: function () {
-    var done = this.async();
-    // var prompts = [{
-    //   type: 'confirm',
-    //   name: 'someAnswer',
-    //   message: 'Would you like to enable this option?',
-    //   default: true
-    // }];
-    //
-    // this.prompt(prompts, function (props) {
-    //   this.props = props;
-    //   // To access props later use this.props.someAnswer;
-    //
-    //   done();
-    // }.bind(this));
-  },
-
-  writing: function () {
+  configuring: function () {
     var baseFiles = [
       '.babelrc',
       '.eslintrc.json',
       '.gitignore',
       '.jscsrc',
       '.scss-lint.yml',
-      '.webpack.config.js',
-      'package.json',
+      'webpack.config.js',
       'LICENCE'
     ];
     for (var i = 0; i < baseFiles.length; ++i) {
