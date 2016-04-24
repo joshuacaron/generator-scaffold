f3bde91a9aad88840f7804b6a4bca38dad64aec7
Joshua Caron <joshua@joshuacaron.ca>
2016-04-23 21:12:58 -0400

    Added package json and trying to get it to work not just when npm linked (won't run off npm currently doesn't write files).

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,39 +2,71 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.Base.extend({
-  configuring: function () {
-    console.log('Writing dotfiles.');
-    this.fs.copy(
-      this.templatePath('LICENCE'),
-      this.destinationPath('LICENCE')
-    );
-    this.fs.copy(
-      this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js')
-    );
-    this.fs.copy(
-      this.templatePath('.scss-lint.yml'),
-      this.destinationPath('.scss-lint.yml')
-    );
-    this.fs.copy(
-      this.templatePath('.jscsrc'),
-      this.destinationPath('.jscsrc')
-    );
-    this.fs.copy(
-      this.templatePath('.gitignore'),
-      this.destinationPath('.gitignore')
-    );
-    this.fs.copy(
-      this.templatePath('.eslintrc.json'),
-      this.destinationPath('.eslintrc.json')
-    );
-    this.fs.copy(
-      this.templatePath('.babelrc'),
-      this.destinationPath('.babelrc')
-    );
-  }
+  prompting: function () {
+    var done = this.async();
 
-  // install: function () {
-  //   this.installDependencies();
-  // }
+    var prompts = [
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Project title',
+        default: 'app'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author',
+        store: true
+      }
+    ];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {title: this.props.title, author: this.props.author}
+      );
+      this.fs.copy(
+        this.templatePath('.babelrc'),
+        this.destinationPath('.babelrc')
+      );
+      this.fs.copy(
+        this.templatePath('webpack.config.js'),
+        this.destinationPath('webpack.config.js')
+      );
+      this.fs.copy(
+        this.templatePath('.eslintrc.json'),
+        this.destinationPath('.eslintrc.json')
+      );
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('.jscsrc'),
+        this.destinationPath('.jscsrc')
+      );
+      this.fs.copy(
+        this.templatePath('.scss-lint.yml'),
+        this.destinationPath('.scss-lint.yml')
+      );
+      this.fs.copy(
+        this.templatePath('LICENCE'),
+        this.destinationPath('LICENCE')
+      );
+    }
+  },
+
+  install: function () {
+    this.npmInstall();
+  }
 });
